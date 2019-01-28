21ac33f0b3d62b3eef93198d4838af5a69d1dd2b
first <matthew.lopez-hays@quinipiac.edu>
2019-01-27 21:11:59 -0500

    basic function definition

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello')
+import pandas as pd
+
+def data_capture():
+    name = input("who is this for: ")
+    streams = int(input("number of revenue streams: "))
+    amount = 0
+    while streams != 0:
+
+        freq = int(input("How many times per month pay is received: "))
+        amount_a = int(input("Amount: "))
+
+        amount += freq * amount_a
+        streams -= 1
+    test = {name: amount}
+    print (test)
+
+data_capture()
